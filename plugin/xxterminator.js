ab6f3ee5cc59a6741042b8ce7e70e2b93c5d7128
mroberts <mroberts@isecpartners.com>
2017-05-08 15:36:53 -0700

    Added parts of the todo

--- plugin/xxterminator.js
+++ plugin/xxterminator.js
@@ -4,9 +4,9 @@
     mutations.forEach(function(mutation) {
       mutation.addedNodes.forEach(function(node){
           if(node.nodeType == 1){
-            chrome.runtime.sendMessage({'type': 'dom', 'msg': node.outerHTML}, null);// For now we don't want to use the calback handler
+            chrome.runtime.sendMessage({'type': 'dom', 'msg': node.outerHTML}, null);// For now we don't want to use the callback handler
           } else if (node.nodeType == 3) {
-            chrome.runtime.sendMessage({'type': 'text', 'msg': node.wholeText}, null);// For now we don't want to use the calback handler
+            chrome.runtime.sendMessage({'type': 'text', 'msg': node.wholeText}, null);// For now we don't want to use the callback handler
           }
       });
     });
