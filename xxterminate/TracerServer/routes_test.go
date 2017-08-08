b5f1e5cbc2b397393d7188682ffdf8c2cd2ffb41
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2017-08-08 13:26:28 -0700

    cleaning up some of the helpers to make a better testing framework.
    Signed-off-by: Jacob Ryan Heath <jake.heath@nccgroup.trust>

--- xxterminate/TracerServer/routes_test.go
+++ xxterminate/TracerServer/routes_test.go
@@ -14,8 +14,16 @@ import (
 	"net/http/httptest"
 )
 
+/* Used to order request and their corresponding tests. */
+type RequestTestPair struct {
+	Request *http.Request
+	Test 	func(*httptest.ResponseRecorder, *testing.T)error
+}
+
 /* Testing addTracer with httptest. POST /tracers */
 func TestAddTracer(t *testing.T) {
+	/* ADDING A TRACER */
+	/////////////////////
 	var (
 		trcr_str= "blahblah"
 		url 	= "http://example.com"
@@ -23,181 +31,119 @@ func TestAddTracer(t *testing.T) {
 	)
 	json_str := fmt.Sprintf(`{"TracerString": "%s", "URL": "%s", "Method": "%s"}`,
 		trcr_str, url, method)
-	t.Logf("Sending the following data: %s\n", json_str)
 
-	req, err := http.NewRequest("POST", "http://127.0.0.1:8081/tracers", bytes.NewBuffer([]byte(json_str)))
-	if err != nil {
-		t.Fatalf("Tried to build an HTTP request but got the following error: %+v\n", err)
-	}
-
-	/* Handle all the server boiler plater and run our request on the router. */
-	rr := oneRequestHelper(req, t)
-
-	/* Make sure the status code is 200. */
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("AddTracer returned the wrong status code: got %v, but wanted %v\n", status, http.StatusOK)
-	} else {
-		t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
-	}
-
-	/* Make sure the body is a valid json object. */
-	got := tracer.Tracer{}
-	json.Unmarshal([]byte(rr.Body.String()), &got)
-	t.Logf("Unmarshalled the data in the following way: %+v\n", got)
-	if got.ID != 1 {
-		t.Errorf("The inserted tracer has the wrong ID. Expected 0, got: %d\n", got.ID)
-	}
-	if got.URL.String != url {
-		t.Errorf("The inserted tracer has the wrong URL. Expected: %s, got: %s\n", url, got.URL.String)
-	}
-	if got.Method.String != method {
-		t.Errorf("The inserted tracer has the wrong Method. Expected %s, got: %s\n", method, got.Method.String)
-	}
-}
-
-/* Testing deleteTracer. DELETE /tracers/<tracer_id> */
-func TestDeleteTracer(t *testing.T) {
-	/* Delete any existing database entries */
-	/* TODO: make a testing database. So that we don't delete a bunch of data when we run tests. */
-	deleteDatabase(t)
-	/* Open the database because the init method from main.go won't trigger. */
-	openDatabase()
-	/* Close the database handle. */
-	defer TracerDB.Close()
-		/* Configure the server. */
-	_, handler := configureServer()
-
-	var (
-		trcr_str= "blahblah"
-		add_url = "http://example.com"
-		method  = "GET"
-	)
-	json_str := fmt.Sprintf(`{"TracerString": "%s", "URL": "%s", "Method": "%s"}`,
-		trcr_str, add_url, method)
-	t.Logf("Sending the following data: %s\n", json_str)
-
-	/* First, add a tracer. */
+	/* Make the POST request. */
 	add_req, err := http.NewRequest("POST", "http://127.0.0.1:8081/tracers", bytes.NewBuffer([]byte(json_str)))
 	if err != nil {
 		t.Fatalf("Tried to build an HTTP request but got the following error: %+v\n", err)
 	}
+	t.Logf("Sending the following data: %s\n", json_str)
+	/* ADDING A TRACER */
+	/////////////////////
 
-	/* Handle all the server boiler plater and run our request on the router. */
-	/* Create a response recorder to capture the response from the route. */
-	rr := httptest.NewRecorder()
-	handler.ServeHTTP(rr, add_req)
+	/* GETING A TRACER */
+	/////////////////////
+	get_req, err := http.NewRequest("GET", "http://127.0.0.1:8081/tracers/1", nil)
+	if err != nil {
+		t.Fatalf("Tried to build an HTTP request but got the following error: %+v\n", err)
+	}
+	/* GETTING A TRACER */
+	/////////////////////
 
-	/* Make sure the status code is 200. */
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("AddTracer returned the wrong status code: got %v, but wanted %v\n", status, http.StatusOK)
-	}
+	/* Create a mapping of the request/test and use the server helper to execute it. */
+	tests := make([]RequestTestPair, 2)
+	add_req_test := RequestTestPair{add_req, addTest}
+	get_req_test := RequestTestPair{get_req, getTest}
+	tests[0] = add_req_test
+	tests[1] = get_req_test
+	serverTestHelper(tests, t)
+}
 
-	/* Make sure the body is a valid tracer object. */
-	got := tracer.Tracer{}
-	json.Unmarshal([]byte(rr.Body.String()), &got)
-	t.Logf("Unmarshalled the data in the following way: %+v\n", got)
-	if got.ID != 1 {
-		t.Errorf("The inserted tracer has the wrong ID. Expected 0, got: %d\n", got.ID)
-	}
-	if got.URL.String != add_url {
-		t.Errorf("The inserted tracer has the wrong URL. Expected: %s, got: %s\n", add_url, got.URL.String)
-	}
-	if got.Method.String != method {
-		t.Errorf("The inserted tracer has the wrong Method. Expected %s, got: %s\n", method, got.Method.String)
-	}
+/* Testing deleteTracer. DELETE /tracers/<tracer_id> */
+func TestDeleteTracer(t *testing.T) {
+	/* ADDING A TRACER */
+	/////////////////////
+	var (
+		trcr_str= "blahblah"
+		url = "http://example.com"
+		method  = "GET"
+		del_url = "http://127.0.0.1:8081/tracers/1"
+		add_url = "http://127.0.0.1:8081/tracers"
+		json_str= fmt.Sprintf(`{"TracerString": "%s", "URL": "%s", "Method": "%s"}`,
+			trcr_str, url, method)
+	)
 
-	/* Build the DELETE and GET request methods for testing. */
-	del_url := fmt.Sprintf("http://127.0.0.1:8081/tracers/%d", got.ID)
+	t.Logf("Sending the following data: %s\n", json_str)
+	add_req, err := http.NewRequest("POST", add_url, bytes.NewBuffer([]byte(json_str)))
+	if err != nil {
+		t.Fatalf("Tried to build an HTTP request but got the following error: %+v\n", err)
+	}
+	/* ADDING A TRACER */
+	/////////////////////
+
+	/* DELETING A TRACER */
+	/////////////////////
 	del_req, err := http.NewRequest("DELETE", del_url, nil)
 	if err != nil {
 		t.Fatalf("Tried to build an HTTP request but got the following error: %+v\n", err)
 	}
 
-	/* Last, double check by making a GET request for the element and verifying the server doesn't serve it again. */
+	del_test := func(rr *httptest.ResponseRecorder, t *testing.T) error {
+		if status := rr.Code; status != http.StatusAccepted {
+			return fmt.Errorf("DeleteTracer returned the wrong status code. Got %v, but wanted %v\n", status, http.StatusAccepted)
+		} else {
+			t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
+		}
+		expected := `{"id": "1", "status": "deleted"}`
+		if rr.Body.String() != expected {
+			return fmt.Errorf("DeleteTracer returned the wrong body in the response. Got %s, but expected %s\n", rr.Body.String(), expected)
+		}
+
+		/* Return nil to indicate no problems. */
+		return nil
+	}
+	/* DELETING A TRACER */
+	/////////////////////
+
+	
+	/* GETTING A TRACER */
+	/////////////////////
 	get_req, err := http.NewRequest("GET", del_url, nil)
 	if err != nil {
 		t.Fatalf("Tried to build an HTTP request but got the following error: %+v\n", err)
 	}
 
-	/* Reset the recorder. */
-	rr = httptest.NewRecorder()
+	get_not_test := func(rr *httptest.ResponseRecorder, t *testing.T) error {
+		if status := rr.Code; status != http.StatusNoContent {
+			return fmt.Errorf("GetTracer returned the wrong status code. Got %v, but wanted %v\n", status, http.StatusNoContent)
+		} else {
+			t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
+		}
 
-	/* Make the GET request to verify we added a new tracer. */
-	handler.ServeHTTP(rr, get_req)
+		/* Validate the server did not leak any data. */
+		got := tracer.Tracer{}
+		json.Unmarshal([]byte(rr.Body.String()), &got)
+		if got.ID != 0 {
+			return fmt.Errorf("GetTracer returned the wrong body in the response. Got %+v, but expected %+v\n", got, tracer.Tracer{})
+		}
 
-	/* Validate the server's response. */
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("GetTracer returned the wrong status code. Got %v, but wanted %v\n", status, http.StatusNoContent)
-	} else {
-		t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
+		/* Return nil to indicate no problems. */
+		return nil
 	}
+	/* GETTING A TRACER */
+	/////////////////////
 
-	/* Validate the tracer was the first tracer inserted. */
-	got = tracer.Tracer{}
-	json.Unmarshal([]byte(rr.Body.String()), &got)
-	if got.ID != 1 {
-		t.Errorf("GetTracer returned the wrong body in the response. Got ID of %+v, but expected %+v\n", got.ID, 1)
-	}
-
-	/* Reset the recorder. */
-	rr = httptest.NewRecorder()
-	/* Server the DELETE request. */
-	handler.ServeHTTP(rr, del_req)
-
-	/* Validate the server response. */
-	if status := rr.Code; status != http.StatusAccepted {
-		t.Errorf("DeleteTracer returned the wrong status code. Got %v, but wanted %v\n", status, http.StatusAccepted)
-	} else {
-		t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
-	}
-	expected := `{"id": "1", "status": "deleted"}`
-	if rr.Body.String() != expected {
-		t.Errorf("DeleteTracer returned the wrong body in the response. Got %s, but expected %s\n", rr.Body.String(), expected)
-	}
-
-	/* Reset the recorder. */
-	rr = httptest.NewRecorder()
-	/* Last, double check by making the same GET request from before and verifying
-	 * the server doesn't serve the tracer again. */
-	handler.ServeHTTP(rr, get_req)
-
-	/* Validate the server response. */
-	if status := rr.Code; status != http.StatusNoContent {
-		t.Errorf("GetTracer returned the wrong status code. Got %v, but wanted %v\n", status, http.StatusNoContent)
-	} else {
-		t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
-	}
-
-	/* Validate the server did not leak any data. */
-	got = tracer.Tracer{}
-	json.Unmarshal([]byte(rr.Body.String()), &got)
-	if got.ID != 0 {
-		t.Errorf("GetTracer returned the wrong body in the response. Got %+v, but expected %+v\n", got, tracer.Tracer{})
-	}
-}
-
-/* Helper function for starting the server and shutting it down. Pass it a function
- * and it will run it for you and throw the errors. */
-func oneRequestHelper(req *http.Request, t *testing.T) *httptest.ResponseRecorder {
-	/* Delete any existing database entries */
-	/* TODO: make a testing database. So that we don't delete a bunch of data when we run tests. */
-	deleteDatabase(t)
-	/* Open the database because the init method from main.go won't trigger. */
-	openDatabase()
-	/* Close the database handle. */
-	defer TracerDB.Close()
-
-	/* Create a response recorder to capture the response from the route. */
-	rr := httptest.NewRecorder()
-	/* Configure the server. */
-	_, handler := configureServer()
-
-	/* Call the request against the handler function. There is no need for a server. */
-	handler.ServeHTTP(rr, req)
-
-
-	/* Return the response, so the caller can test the results. */
-	return rr
+	/* Create a slice of the RequestTestPairs and use the server helper to execute them. */
+	tests := make([]RequestTestPair, 4)
+	add_req_test := RequestTestPair{add_req, addTest}
+	get_req_test := RequestTestPair{get_req, getTest}
+	del_req_test := RequestTestPair{del_req, del_test}
+	get_not_req_test := RequestTestPair{get_req, get_not_test}
+	tests[0] = add_req_test
+	tests[1] = get_req_test
+	tests[2] = del_req_test
+	tests[3] = get_not_req_test
+	serverTestHelper(tests, t)
 }
 
 /* Delete any existing database */
@@ -216,4 +162,79 @@ func deleteDatabase(t *testing.T) {
 			t.Errorf("Wasn't able to delete the database at: %s\n", db_loc)
 		}
 	}
+}
+
+/* A function that takes a map of requests to test functions. Each request is run in 
+ * sequence and each test is used to validate the response. This function can be used to 
+ * chain request/response tests together, for example to test if a particular resource
+ * has been deleted or created. */
+func serverTestHelper(tests []RequestTestPair, t *testing.T) {
+	/* Delete any existing database entries */
+	/* TODO: make a testing database. So that we don't delete a bunch of data when we run tests. */
+	deleteDatabase(t)
+	/* Open the database because the init method from main.go won't trigger. */
+	openDatabase()
+	/* Close the database handle. */
+	defer TracerDB.Close()
+	_, handler := configureServer()
+
+	for _, pair := range tests {
+		/* For each request/test combo:
+		* 1.) send the request
+		* 2.) collect the response
+		* 3.) run the response on the test method
+		* 4.) break on error */
+		rr := httptest.NewRecorder()
+		handler.ServeHTTP(rr, pair.Request)
+		err := pair.Test(rr, t)
+		if err != nil {
+			t.Errorf("The following request, %+v, did not pass it's test: %+v\n", pair.Request, err)
+		}
+	}
+}
+
+/* Commonly used GET request test. */
+func getTest(rr *httptest.ResponseRecorder, t *testing.T) error {
+	if status := rr.Code; status != http.StatusOK {
+		return fmt.Errorf("GetTracer returned the wrong status code. Got %v, but wanted %v\n", status, http.StatusNoContent)
+	} else {
+		t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
+	}
+
+	/* Validate the tracer was the first tracer inserted. */
+	got := tracer.Tracer{}
+	json.Unmarshal([]byte(rr.Body.String()), &got)
+	if got.ID != 1 {
+		return fmt.Errorf("GetTracer returned the wrong body in the response. Got ID of %+v, but expected %+v\n", got.ID, 1)
+	}
+
+	/* Return nil to indicate no problems. */
+	return nil
+}
+
+/* Commonly used POST request test. */
+func addTest(rr *httptest.ResponseRecorder, t *testing.T) error {
+	/* Make sure the status code is 200. */
+	if status := rr.Code; status != http.StatusOK {
+		return fmt.Errorf("AddTracer returned the wrong status code: got %v, but wanted %v\n", status, http.StatusOK)
+	} else {
+		t.Logf("Status good. Got : %+v, Body: %s\n", status, rr.Body.String())
+	}
+
+	/* Make sure the body is a valid json object. */
+	got := tracer.Tracer{}
+	json.Unmarshal([]byte(rr.Body.String()), &got)
+	t.Logf("Unmarshalled the data in the following way: %+v\n", got)
+	if got.ID != 1 {
+		return fmt.Errorf("The inserted tracer has the wrong ID. Expected 0, got: %d\n", got.ID)
+	}
+	if got.URL.String == "" {
+		return fmt.Errorf("The inserted tracer has the wrong URL. Got: %s\n", got.URL.String)
+	}
+	if got.Method.String == "" {
+		return fmt.Errorf("The inserted tracer has the wrong Method. Got: %s\n", got.Method.String)
+	}
+
+	/* Return nil to indicate nothing went wrong. */
+	return nil
 }
