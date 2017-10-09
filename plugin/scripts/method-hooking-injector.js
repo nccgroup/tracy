5d743065ea3b2583fe664c888960cc952133f162
mroberts <mroberts@isecpartners.com>
2017-10-09 11:34:00 -0700

    Added textareas support Reduced image size Added image to manifest rewrote background-image to use image urls

--- plugin/scripts/method-hooking-injector.js
+++ plugin/scripts/method-hooking-injector.js
@@ -11,7 +11,7 @@
   //Inject our hooks now that we have a global varable we can refer to
   var hookInjector = document.createElement("script");
   hookInjector.type = "text/javascript";
-  hookInjector.src = "chrome-extension://" + chrome.runtime.id +  "/scripts/method-hooking.js";//Change this to chrome.extension.getURL
+  hookInjector.src = chrome.runtime.getURL("/scripts/method-hooking.js");
   document.documentElement.appendChild(hookInjector);
   hookInjector.parentNode.removeChild(hookInjector);
 })();
