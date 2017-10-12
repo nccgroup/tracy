204d8f4cde10b827a73d0590d0640939414500d1
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2017-10-12 14:52:59 -0700

    adding the build folder
    Signed-off-by: Jacob Ryan Heath <jake.heath@nccgroup.trust>

--- tracer/view/xxterminate/src/TracerNavigation.js
+++ tracer/view/xxterminate/src/TracerNavigation.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 class TracerNavigation extends React.Component {
-	constructor(props){
-		super(props);
-	}
 	render() {
 		return (
 			<div className="navigation-arrows">
-				<div onClick={this.props.goToPreviousReference} className="glyphicon glyphicon-arrow-up"></div>
+				<div 
+					onClick={this.props.goToPreviousReference} 
+					className="glyphicon glyphicon-arrow-up">
+				</div>
 				<br></br>
-				<div onClick={this.props.goToNextReference} className="glyphicon glyphicon-arrow-down"></div>
+				<div 
+					onClick={this.props.goToNextReference} 
+					className="glyphicon glyphicon-arrow-down">
+				</div>
 			</div>);
 	}
 }
