6d309ae8f4c675074736fe8501332ce6a273adae
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2018-04-16 14:09:06 -0700

    fixing the bindata again and removing the build dir from the repo
    Signed-off-by: Jacob Ryan Heath <jake.heath@nccgroup.trust>

---
+++ api/rest/bindata_assetfs.go
@@ -0,0 +1,10 @@
+package rest
+
+import (
+	"github.com/elazarl/go-bindata-assetfs"
+)
+
+/* Nop used for builds. This implementation is fulfilled when the binary is build. */
+func assetFS() *assetfs.AssetFS {
+	panic("Don't call me. Use the debug-ui flag instead.")
+}
