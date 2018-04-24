afd2dfd8f247e076017e135465c6c9a1a278909f
mroberts <mroberts@isecpartners.com>
2018-04-24 11:46:06 -0700

    added new table

---
+++ api/types/raw_event_types.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+/*RawEvent is a payload type that can be used by the extension. */
+type RawEvent struct {
+	gorm.Model
+	RawData string `json:"RawData" gorm:"not null"`
+	events  []TracerEvent
+}
