f7d78e1c4ef29eba680f2d9713f2713b059ad819
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2018-06-07 11:02:48 -0700

    hooking innerhtml
    Signed-off-by: Jacob Ryan Heath <jake.heath@nccgroup.trust>

--- plugin/scripts/method-hooking-injector.js
+++ plugin/scripts/method-hooking-injector.js
@@ -1,17 +1,27 @@
 (function() {
-
-  window.addEventListener("message", (event) => {
-
-    if (event.source != window)
-        return;
-
-    chrome.runtime.sendMessage({'type': event.data.type, 'msg': event.data.msg,"location": document.location.href}, null);
+  // Create a listener on the shared window between content scripts and injected
+  // scripts so that injected scripts can talk to the extension via window.postMessage.
+  window.addEventListener("message", event => {
+    chrome.runtime.sendMessage(event.data);
   });
 
-  //Inject our hooks now that we have a global varable we can refer to
-  var hookInjector = document.createElement("script");
-  hookInjector.type = "text/javascript";
-  hookInjector.src = chrome.runtime.getURL("/scripts/method-hooking.js");
-  document.documentElement.appendChild(hookInjector);
-  hookInjector.parentNode.removeChild(hookInjector);
+  // A list of scripts we want to inject into the page rather than have them as a
+  // content script.
+  const injectionScripts = ["innerhtml.js"];
+  // Inject the scripts.
+  injectionScripts.map(getInjectionSrc).map(injectScript);
+
+  /*getInjectionSrc returns the file path of the script we are trying to inject */
+  function getInjectionSrc(file) {
+    return chrome.runtime.getURL(`scripts/${file}`);
+  }
+
+  /*injectScript injects the script into the page and then removes it. */
+  function injectScript(src) {
+    const hookInjector = document.createElement("script");
+    hookInjector.type = "text/javascript";
+    hookInjector.src = src;
+    document.documentElement.appendChild(hookInjector);
+    hookInjector.parentNode.removeChild(hookInjector); // TODO: do we need to remove it?
+  }
 })();
