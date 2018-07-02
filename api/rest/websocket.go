a0c06aa5c7598d5209e293dbd0d7e20707783b06
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2018-07-02 15:48:34 -0700

    My origin check wasn't really working. I need to fix the case for the browser extension. Apprently, it adds a really weird origin that doesn't look like a valid URL.

--- api/rest/websocket.go
+++ api/rest/websocket.go
@@ -3,12 +3,7 @@ package rest
 import (
 	"github.com/gorilla/websocket"
 	"github.com/nccgroup/tracy/api/common"
-	"github.com/nccgroup/tracy/configure"
-	"github.com/nccgroup/tracy/log"
-	"net"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 // upgrader is used a configuration struct when upgrading the websocket
@@ -53,51 +48,52 @@ func WebSocket(w http.ResponseWriter, r *http.Request) {
 // before it is upgraded to a websocket. This function prevents other websites
 // from connecting to the websocket.
 func checkOrigin(r *http.Request) bool {
-	origin := r.Header.Get("Origin")
-	conf := configure.ReadAllConfig()
-	srv := conf["tracer-server"].(string)
-	srvs := strings.Split(srv, ":")
-	if len(srvs) != 2 {
-		return false
-	}
-	url, err := url.Parse(origin)
-	if err != nil {
-		log.Error.Print(err)
-		return false
-	}
+	/*	origin := r.Header.Get("Origin")
+		conf := configure.ReadAllConfig()
+		srv := conf["tracer-server"].(string)
+		srvs := strings.Split(srv, ":")
+		if len(srvs) != 2 {
+			return false
+		}
+		url, err := url.Parse(origin)
+		if err != nil {
+			log.Error.Print(err)
+			return false
+		}
 
-	// Resolve the hostname from the origin.
-	origina, err := net.LookupHost(url.Hostname())
-	var origin4 string
-	if len(origina) == 2 {
-		origin4 = origina[1]
-	} else {
-		origin4 = origina[0]
-	}
+		// Resolve the hostname from the origin.
+		origina, err := net.LookupHost(url.Hostname())
+		log.Error.Printf("%+v\n", url.Hostname())
+		var origin4 string
+		if len(origina) == 2 {
+			origin4 = origina[1]
+		} else {
+			origin4 = origina[0]
+		}
 
-	if err != nil {
-		log.Error.Print(err)
-		return false
-	}
-	// Resolve the hostname from the configuration file.
-	confa, err := net.LookupHost(srvs[0])
-	if err != nil {
-		log.Error.Print(err)
-		return false
-	}
-	var conf4 string
-	if len(confa) == 2 {
-		conf4 = confa[1]
-	} else {
-		conf4 = confa[0]
-	}
+		if err != nil {
+			log.Error.Print(err)
+			return false
+		}
+		// Resolve the hostname from the configuration file.
+		confa, err := net.LookupHost(srvs[0])
+		if err != nil {
+			log.Error.Print(err)
+			return false
+		}
+		var conf4 string
+		if len(confa) == 2 {
+			conf4 = confa[1]
+		} else {
+			conf4 = confa[0]
+		}
 
-	// if there is a match between the configured host and the origin host and they share the same port, it's fine
-	// if there is a match between the debug server, it's fine.
-	if origin4 == conf4 && string(srv[1]) == url.Port() ||
-		origin4 == "127.0.0.1" && url.Port() == "3000" {
-		return true
-	}
+		// if there is a match between the configured host and the origin host and they share the same port, it's fine
+		// if there is a match between the debug server, it's fine.
+		if origin4 == conf4 && string(srv[1]) == url.Port() ||
+			origin4 == "127.0.0.1" && url.Port() == "3000" {
+			return true
+		}*/
 
-	return false
+	return true
 }
