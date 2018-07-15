cb534f60546225c9caabf46816672c87d2f962b0
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2018-07-15 15:40:42 -0700

    code review of proxy.go completed

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -20,10 +20,14 @@ import (
 	"github.com/nccgroup/tracy/log"
 )
 
+// Proxy is the object that stores the configured proxy TCP listener
+// for the tracy proxy.
 type Proxy struct {
 	Listener net.Listener
 }
 
+// Accept is the wrapper function for accepting TCP connections for
+// the proxy.
 func (p *Proxy) Accept() {
 	for {
 		conn, err := p.Listener.Accept()
@@ -37,162 +41,271 @@ func (p *Proxy) Accept() {
 
 }
 
+// New instantiates a Proxy object with the passed in net.Listener.
 func New(listener net.Listener) *Proxy {
 	return &Proxy{
 		Listener: listener,
 	}
 }
 
+// identifyRequestsForGenereateTracer is a helper that looks for generated
+// tracer payloads that might not have had a request associated with them
+// yet. When the proxy finds one, it associates that HTTP request with the
+// payload so that there is a record of where the input was sent to the
+// server for the fist time.
+func identifyRequestsforGeneratedTracer(dump, method string) {
+	// TODO: probably should change this to a websocket notifier so that we
+	// don't have to do this database lookup.
+	tracersBytes, err := common.GetTracers()
+	if err != nil {
+		log.Error.Print(err)
+		return
+	}
+
+	var requests []types.Request
+	err = json.Unmarshal(tracersBytes, &requests)
+	if err != nil {
+		log.Error.Print(err)
+		return
+	}
+
+	for _, req := range requests {
+		for _, tracer := range req.Tracers {
+			if !strings.Contains(dump, tracer.TracerPayload) ||
+				req.RawRequest != "GENERATED" {
+				continue
+			}
+			req.RawRequest = dump
+			req.RequestMethod = method
+
+			err = store.DB.Save(&req).Error
+			if err != nil {
+				log.Error.Print(err)
+				return
+			}
+			common.UpdateSubscribers(req)
+		}
+	}
+}
+
+// identifyTracersInResponse looks for all the registered tracers in an HTTP
+// response and makes an event for each of them.
+func identifyTracersInResponse(b []byte, host string, request *http.Request, resp *http.Response) {
+	// Check if the host is the tracer API server. We don't want to trigger
+	// anything if we accidentally proxied a tracer server API call because
+	// it will cause a recursion.
+	if configure.ServerInWhitelist(host) {
+		return
+	}
+
+	// Check that the server actually sent compressed data
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		reader, err := gzip.NewReader(bytes.NewReader(b))
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Error.Print(err)
+			return
+		}
+
+		if reader == nil {
+			return
+		}
+
+		defer reader.Close()
+		b, err = ioutil.ReadAll(reader)
+
+	}
+
+	requestsJSON, err := common.GetTracers()
+	if err != nil {
+		log.Error.Print(err)
+		return
+	}
+
+	var requests []types.Request
+	err = json.Unmarshal(requestsJSON, &requests)
+	if err != nil {
+		log.Error.Print(err)
+		return
+	}
+
+	if len(requests) == 0 {
+		return
+	}
+
+	url := request.Host + request.RequestURI
+	tracers := findTracersInResponseBody(string(b), url, requests)
+
+	if len(tracers) == 0 {
+		return
+	}
+
+	// We have to do this first so that we can get the ID of the raw event
+	// and insert it with the event structure.
+	rawEvent, err := common.AddEventData(string(b))
+	if err != nil {
+		log.Error.Print(err)
+		return
+	}
+
+	for _, tracer := range tracers {
+		for _, event := range tracer.TracerEvents {
+			//TODO: should probably make a bulk add events function
+			event.RawEventID = rawEvent.ID
+			event.RawEvent = rawEvent
+
+			if err = store.DB.First(&tracer, "id = ?", event.TracerID).Error; err != nil {
+				// Don't print errors about the key being unique in the DB.
+				if !strings.Contains(err.Error(), "UNIQUE") {
+					log.Error.Println(err)
+					return
+				}
+			}
+			_, err = common.AddEvent(tracer, event)
+			if err != nil {
+				log.Error.Print(err)
+				return
+			}
+		}
+	}
+}
+
+// handleConnect handles the upgrade requests for 'CONNECT' HTTP requests.
+func handleConnect(client net.Conn, request *http.Request, host string) (net.Conn, *http.Request, bool, error) {
+	// Try to upgrade the 'CONNECT' request to a TLS connection with
+	// the configured certificate.
+	c, isHTTPS, err := upgradeConnectionTLS(client, host)
+	if err != nil {
+		log.Warning.Println(err)
+		return nil, nil, false, err
+	}
+
+	// After the connection has been upgraded, reread the request
+	// structure over TLS.
+	request, err = http.ReadRequest(bufio.NewReader(c))
+
+	if err != nil {
+		log.Warning.Println(err)
+		return nil, nil, false, err
+	}
+
+	// Read the request structure as a slice of bytes.
+	if log.Verbose {
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			log.Warning.Println(err)
+			return nil, nil, false, err
+		}
+
+		log.Trace.Print(string(dump))
+	}
+
+	return c, request, isHTTPS, nil
+}
+
 // handleConnection handles any TCP connections to the proxy. Client refers to
 // the client making the connection to the proxy. Server refers to the actual
 // server they are attempting to connect to after going through the proxy.
 func handleConnection(client net.Conn) {
-	/* Read a request structure from the TCP connection. */
+	// Read a request structure from the TCP connection.
 	request, err := http.ReadRequest(bufio.NewReader(client))
-
 	if err == io.EOF {
 		return
 	}
-
 	if err != nil {
-		log.Error.Println(err)
+		log.Error.Print(err)
 		return
 	}
 
 	host := request.URL.Host
 	isHTTPS := false
 
-	/* Dump the request structure as a slice of bytes. */
+	// Dump the request structure as a slice of bytes.
 	if log.Verbose {
-		dump, _ := httputil.DumpRequest(request, true) //This should only run if in debug mode
+		dump, _ := httputil.DumpRequest(request, true)
 		log.Trace.Println(string(dump))
 	}
 
-	/* If the request method is `CONNECT`, it's either a TLS connection or a websocket. */
+	// If the request method is 'CONNECT', it's either a TLS connection or a
+	// websocket.
 	if request.Method == http.MethodConnect {
-		/* Try to upgrade the `CONNECT` request to a TLS connection with the configured certificate. */
-		client, isHTTPS, err = upgradeConnectionTLS(client, host)
+		client, request, isHTTPS, err = handleConnect(client, request, host)
 		if err == io.EOF {
 			return
 		}
-
 		if err != nil {
-			log.Error.Println(err)
+			log.Error.Print(err)
 			return
 		}
-
-		/* After the connection has been upgraded, reread the request structure over TLS. */
-		request, err = http.ReadRequest(bufio.NewReader(client))
-
-		if err == io.EOF {
-			return
-		}
-
-		if err != nil {
-			log.Error.Println(err)
-			return
-		}
-
-		/* Read the request structure as a slice of bytes. */
-		if log.Verbose {
-			dump, err := httputil.DumpRequest(request, true) //This should only run in debug mode
-			if err != nil {
-				log.Error.Println(err)
-				return
-			}
-
-			log.Trace.Println(string(dump))
-		}
-
 	}
 
-	/* Moving the client close here for the same reason as request, above. */
+	// Moving the client close here for the same reason as request, above.
 	if client != nil {
 		defer client.Close()
 	}
 
-	// This custom header is used to tell tracy to not do any swapping of values.
+	// Requests with the custom HTTP header "X-TRACY" are opting out of the
+	// swapping of tracy payloads. Also, we keep a whitelist of hosts that
+	// shouldn't swap out tracy payloads so that recursion issues don't
+	// happen. For example, tracy payloads will occur all over the UI and
+	// we don't want those to be swapped.
 	if !configure.ServerInWhitelist(host) && request.Header.Get("X-TRACY") == "" {
-		// Look for tracers that might have been generated out of band using the API.
-		// Do this by checking if there exists a tracer, but we have no record of which
-		// request it came from.
+		// Look for tracers that might have been generated out of band
+		// using the API. Do this by checking if there exists a tracer,
+		// but we have no record of which request it came from.
 		dump, err := httputil.DumpRequest(request, true)
-		method := request.Method
-		go func() {
-			if err == nil {
-				dumpStr := string(dump)
-				var tracersBytes []byte
-				// TODO: probably should change this to a websocket notifier so that we don't have to do this
-				// database lookup.
-				tracersBytes, err = common.GetTracers()
-				if err == nil {
-					var requests []types.Request
-					err = json.Unmarshal(tracersBytes, &requests)
-					if err == nil {
-						for _, req := range requests {
-							for _, tracer := range req.Tracers {
-								if strings.Contains(dumpStr, tracer.TracerPayload) && req.RawRequest == "GENERATED" {
-									req.RawRequest = dumpStr
-									req.RequestMethod = method
+		if err != nil {
+			log.Error.Print(err)
+			return
+		}
 
-									//Update the record
-									err = store.DB.Save(&req).Error
-									if err == nil {
-										common.UpdateSubscribers(req)
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+		go identifyRequestsforGeneratedTracer(string(dump), request.Method)
 
-			if err != nil {
-				log.Error.Println(err)
-			}
-
-		}()
-
-		/* Search through the request for the tracer keyword. */
+		// Search through the request for the tracer keyword.
 		tracers, err := replaceTracers(request)
 
 		if err != nil {
-			/* If there was an error replacing the tracers, fail fast and leave. */
 			log.Error.Println(err)
 			return
 		}
 
-		/* Check if the host is the tracer API server. We don't want to trigger anything if we accidentally proxied a tracer
-		 * server API call because it will trigger a recursion. */
-		go func() {
-			if !configure.ServerInWhitelist(host) && len(tracers) > 0 {
+		// Check if the host is the tracer API server. We don't want to
+		// trigger anything if we accidentally proxied a tracer server
+		// API call because it will cause a recursion.
+		if !configure.ServerInWhitelist(host) && len(tracers) > 0 {
+			go func() {
+				// Have to do this again because we changed the
+				// contents of the request and the request object
+				// is a pointer.
 				dump, err := httputil.DumpRequest(request, true)
-				if err == nil {
-					req := types.Request{
-						RawRequest:    string(dump),
-						RequestURL:    request.Host + request.RequestURI,
-						RequestMethod: request.Method,
-						Tracers:       tracers,
-					}
-
-					/* Use the API to add each tracer events to their corresponding tracer. */
-					_, err = common.AddTracer(req)
-				}
-
 				if err != nil {
-					log.Error.Println(err)
+					log.Error.Print(err)
+					return
 				}
-			}
-		}()
+
+				req := types.Request{
+					RawRequest:    string(dump),
+					RequestURL:    request.Host + request.RequestURI,
+					RequestMethod: request.Method,
+					Tracers:       tracers,
+				}
+
+				_, err = common.AddTracer(req)
+				if err != nil {
+					log.Error.Print(err)
+				}
+			}()
+		}
 	}
 
 	var server net.Conn
 
-	/* Based on the scheme, the API is different to backside of the proxy connection. */
-	//TODO: I think we can change the default transport here to timeout a bit faster and to
-	//use connections
+	// Based on the scheme, the API is different to backside of the proxy connection.
+	// TODO: I think we can change the default transport here to timeout a
+	// bit faster and to reuse connections.
 	if !isHTTPS {
-
 		if strings.Index(host, ":") == -1 {
 			server, err = net.Dial("tcp", host+":80")
 		} else {
@@ -203,18 +316,22 @@ func handleConnection(client net.Conn) {
 			defer server.Close()
 		}
 
-		/* Fail fast if the connection to the backside of the proxy failed. */
 		if err != nil {
 			log.Error.Println(err)
 			return
 		}
 	} else {
 		var tserver *tls.Conn
-		/* If the scheme is HTTPS, need to the use the tls package to make the dial. */
-		tserver, err = tls.Dial("tcp", host, &tls.Config{InsecureSkipVerify: true})
+		// If the scheme is HTTPS, need to the use the tls package to
+		// make the dial. We also don't care about insecure connections
+		// when using tracy. A lot the apps we are testing use dev or
+		// QA environments with self-signed certificates.
+		tserver, err = tls.Dial("tcp", host, &tls.Config{
+			InsecureSkipVerify: true,
+		})
 
-		// Have to check for nil differently with tls.Dial because it return a pointer
-		// of a connection instead of a struct
+		// Have to check for nil differently with tls.Dial because it
+		// returns a pointer of a connection instead of a struct.
 		var nilTest *tls.Conn
 		if tserver != nilTest {
 			server = tserver
@@ -231,10 +348,11 @@ func handleConnection(client net.Conn) {
 		}
 	}
 
-	/* Write the entire request to the backside connection of the proxy. */
+	// Write the entire request to the backside connection of the proxy.
 	request.Write(server)
 
 	resp, err := http.ReadResponse(bufio.NewReader(server), nil)
+
 	if resp != nil {
 		defer resp.Body.Close()
 	}
@@ -251,98 +369,40 @@ func handleConnection(client net.Conn) {
 		return
 	}
 
-	/* Search for any known tracers in the response. Since the list of tracers might get large, perform this operation
-	 * in a goroutine. The proxy can finish this connection before this finishes. */
-	go func() {
-		/* Check if the host is the tracer API server. We don't want to trigger anything if we accidentally proxied a tracer
-		 * server API call because it will trigger a recursion. */
-		if !configure.ServerInWhitelist(host) {
-			// Check that the server actually sent compressed data
-			var err error
-			if resp.Header.Get("Content-Encoding") == "gzip" {
-				// Note that we are only reading the response body. No longer a need to manually filter it out
-				var reader io.ReadCloser
-				if reader, err = gzip.NewReader(bytes.NewReader(b)); err == nil && reader != nil {
-					defer reader.Close()
-					b, err = ioutil.ReadAll(reader)
-				}
-			}
+	// Search for any known tracers in the response. Since the list of tracers
+	// might get large, perform this operation in a goroutine.
+	// The proxy can finish this connection before this finishes.
+	go identifyTracersInResponse(b, host, request, resp)
 
-			if err == nil {
-				/* Get a current list of the tracers so they can be searched for. */
-				var requestsJSON []byte
-				requestsJSON, err = common.GetTracers()
-				if err == nil {
-					requests := []types.Request{}
-					err = json.Unmarshal(requestsJSON, &requests)
-					if err == nil {
-						if len(requests) != 0 {
-							url := request.Host + request.RequestURI
-							tracers := findTracersInResponseBody(string(b), url, requests)
-							/* Use the API to add each tracer events to their corresponding tracer. */
-
-							if len(tracers) > 0 {
-								/* We have to do this first so that we can get the ID of the raw event
-								 * and insert it with the event structure. */
-
-								var rawEvent types.RawEvent
-								if rawEvent, err = common.AddEventData(string(b)); err == nil {
-									for _, tracer := range tracers {
-										for _, event := range tracer.TracerEvents {
-											//TODO: should probably make a bulk add events function
-											event.RawEventID = rawEvent.ID
-											event.RawEvent = rawEvent
-
-											//TODO: need to test this.
-											if err = store.DB.First(&tracer, "id = ?", event.TracerID).Error; err != nil {
-												log.Error.Println(err)
-												return
-											}
-											_, err = common.AddEvent(tracer, event)
-										}
-									}
-								}
-							}
-
-						}
-					}
-				}
-			}
-
-			if err == io.EOF {
-				return
-			}
-
-			if err != nil {
-				if !strings.Contains(err.Error(), "UNIQUE") {
-					log.Error.Println(err)
-				}
-			}
-		}
-	}()
-
-	/* Right the response back to the client. */
+	// Right the response back to the client.
 	resp.Body = ioutil.NopCloser(&save)
 	resp.Write(client)
 
-	/* If the backside of the proxy tries to change the protocol, forward this change and simply copy the bytes
-	 * between the two connection until they are finished. */
+	// If the backside of the proxy tries to change the protocol, forward
+	// this change and simply copy the bytes between the two connections
+	//until they are finished.
 	if resp.StatusCode == http.StatusSwitchingProtocols {
-		/* Bridge blocks until the connections are finished talking, so client-to-server communication needs to
-		 * be in its own goroutine. */
+		// Bridge blocks until the connections are finished talking, so
+		// client-to-server communication needs to be in its own goroutine.
 		go bridge(client, server)
-		/* Block on server-to-client communication until both connections are finished. */
+		// Block on server-to-client communication until both connections
+		// are finished.
 		bridge(server, client)
 	}
 }
 
-/* Helper function for reading bytes from one connection and writing them to another connection. */
+// bridge reads bytes from one connection and writes them to another connection.
+// TODO: add code to look for tracy payloads in websockets so that
+// we can identify when sources of input include data coming from the server in
+// websocket.
 func bridge(client net.Conn, server net.Conn) {
 	buf := make([]byte, 1024*4)
 	for {
-		/* Read up to 1024*4 bytes from the client. */
+		// Read up to 1024*4 bytes from the client.
 		nb, err := client.Read(buf)
-		if err == io.EOF || nb == 0 { // As of golang 1.7, 0-byte-reads don't always return EOF
+
+		// As of golang 1.7, 0-byte-reads don't always return EOF
+		if err == io.EOF || nb == 0 {
 			break
 		}
 
@@ -350,11 +410,10 @@ func bridge(client net.Conn, server net.Conn) {
 			log.Error.Println(err)
 		}
 
-		/* Copy the bytes read above to the other connection. */
+		// Copy the bytes read above to the other connection.
 		_, err = server.Write(buf[:nb])
 
 		if err != nil {
-			/* If there was an error, fail fast. */
 			log.Error.Println(err)
 			break
 		}
