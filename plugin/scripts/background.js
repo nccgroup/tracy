146f92026c301d901a1ff9a94ea01eab8189a447
Jacob Ryan Heath <jake.heath@nccgroup.trust>
2018-08-02 12:19:55 -0700

    Adding autofill functionality and context menus to the plugin. This will need some user testing, because autofill makes some apps behave in kind of a weird way. However, thought I would push and get feedback. Right click context menus seem to work well. Also, I have bumped the version number on the manifest and did a little bit of code review in the background page.

--- plugin/scripts/background.js
+++ plugin/scripts/background.js
@@ -1,4 +1,5 @@
-/* Helper function for pushing bulk events to the API. */
+// bulkAddEvents makes a POST request to the bulk events to the API with
+// a set of events from the DOM.
 function bulkAddEvents(events) {
   fetch(`http://${restServer}/tracers/events/bulk`, {
     headers: {
@@ -14,36 +15,37 @@ function bulkAddEvents(events) {
   );
 }
 
-/* Handler function for events triggered from the web page. Events should contain a list of DOM events. This handler
- * searches through each of the DOM events for tracer strings. If it finds a string in the DOM event, it submits
- * an API request to the API server. */
+// requestHandler takes the current set of jobs from the page, filters them
+// against the current set of tracer payloads, and sends them as a batch API
+// request to the API. Events should contain a list of DOM events.
 function requestHandler(domEvents) {
-  /* A filtered list of DOM events based on if the event has a tracer in it. Each DOM event can have multiple tracer
-     * strings. */
+  // A filtered list of DOM events based on if the event has a tracer in it.
+  // Each DOM event can have multiple tracer strings.
   let filteredEvents = [];
 
-  /* For each DOM write, search for all the tracer strings and collect their location. */
+  // For each DOM write, search for all the tracer strings and collect their location.
   for (let domEventKey in domEvents) {
     const domEvent = domEvents[domEventKey];
-    /* Each DOM write could have many tracer strings in it. Group these together. */
+    // Each DOM write could have many tracer strings in it. Group these together.
     let tracersPerDomEvent = [];
 
-    /* The request is a batched list of DOM events. Iterate through each of them looking for a tracer string. */
+    // The request is a batched list of DOM events. Iterate through each of them
+    // looking for a tracer string.
     for (let id in tracerPayloads) {
       const tracerPayload = tracerPayloads[id];
-      /* If a tracer was found, make sure all the event data is proper and add it to the list of tracers found for this event.
-             * Continue to the rest of the recorded. */
+      // If a tracer was found, add it to the list of tracers found for this event.
+      // Continue to the rest of the recorded.
       const tracerLocation = domEvent.msg.indexOf(tracerPayload);
       if (tracerLocation != -1) {
-        /* Add this location data to the list of tracers per DOM event. */
+        // Add this location data to the list of tracers per DOM event.
         tracersPerDomEvent.push(tracerPayload);
       }
     }
 
-    /* After collecting all the tracers per DOM event, add this DOM event to the list of filtered DOM events that
-         * will be submitted in bulk to the event API. */
+    // After collecting all the tracers per DOM event, add this DOM event to the
+    // list of filtered DOM events that will be submitted in bulk to the event API.
     if (tracersPerDomEvent.length > 0) {
-      /* Sanity check the data we are expecting is in the message. */
+      // Sanity check
       if (!domEvent.msg) {
         console.error("The DOM event msg field was not set properly.");
         return;
@@ -68,13 +70,12 @@ function requestHandler(domEvents) {
     }
   }
 
-  /* Send the events to the API. */
   if (filteredEvents.length > 0) {
     bulkAddEvents(filteredEvents);
   }
 }
 
-/* Routes messages from the extension to various functions on the background. */
+// Routes messages from the extension to various functions on the background.
 function messageRouter(message, sender, sendResponse) {
   if (message && message["message-type"]) {
     switch (message["message-type"]) {
@@ -91,49 +92,64 @@ function messageRouter(message, sender, sendResponse) {
   }
 }
 
-/* Refreshes the configuration. */
-function refreshConfig(wsConnect) {
-  chrome.storage.local.get(
-    {
-      restHost: "localhost",
-      restPort: 8081
-    },
-    function(res) {
-      restServer = res.restHost + ":" + res.restPort;
-      fetch(`http://${restServer}/config`, { headers: { Hoot: "!" } })
-        .then(res => res.json())
-        .catch(err => console.error("Error:", error))
-        .then(res => {
-          tracerStringTypes = Object.keys(res["tracers"]);
-          defaultTracer = res["default-tracer"];
-        });
-
-      fetch(`http://${restServer}/tracers`, {
-        headers: { Hoot: "!" }
-      })
-        .then(res => res.json())
-        .catch(error => console.error("Error:", error))
-        .then(res => {
-          return [].concat.apply(
-            null,
-            res.map(r => {
-              [].concat(
-                r.Tracers.map(t => {
-                  return t.TracerPayload;
-                })
-              );
-            })
-          );
-        });
-
-      if (wsConnect) {
-        websocketConnect();
-      }
-    }
+// refresheConfig makes an API request for the latest config from `/config`,
+// pulls configuration from the extension settings page and gets a current
+// list of tracers. refreshConfig is usually called on page load.
+async function refreshConfig(wsConnect) {
+  const settings = await new Promise(resolve =>
+    chrome.storage.local.get({ restHost: "localhost", restPort: 8081 }, res =>
+      resolve(res)
+    )
   );
+
+  restServer = settings.restHost + ":" + settings.restPort;
+  fetch(`http://${restServer}/config`, { headers: { Hoot: "!" } })
+    .then(res => res.json())
+    .catch(err => console.error(err))
+    .then(res => {
+      tracerStringTypes = Object.keys(res["tracers"]);
+      defaultTracer = res["default-tracer"];
+
+      // TODO: can't figure out why Firefox is throwing an error here
+      // about duplicate IDs.
+      tracerStringTypes.forEach(i => {
+        chrome.contextMenus.remove(i, () => {
+          // Context menu for right-clicking on an editable field.
+          chrome.contextMenus.create({
+            id: i,
+            title: i,
+            contexts: ["editable"],
+            onclick: (info, tab) => {
+              chrome.tabs.sendMessage(tab.id, {
+                cmd: "clickCache",
+                tracerString: i
+              });
+            }
+          });
+        });
+      });
+    });
+
+  fetch(`http://${restServer}/tracers`, { headers: { Hoot: "!" } })
+    .then(res => res.json())
+    .catch(err => console.error(err))
+    .then(
+      res =>
+        (tracerPayloads = [].concat.apply(
+          [],
+          res.map(r => [].concat(r.Tracers.map(t => t.TracerPayload)))
+        ))
+    )
+    .catch(err => console.error(err));
+
+  if (wsConnect) {
+    websocketConnect();
+  }
 }
 
-/* Connect to the websocket endpoint so we don't have to poll for new tracer strings. */
+// TODO: consider getting rid of this websocket so that we can do away with persistent
+// background page.
+// Connect to the websocket endpoint so we don't have to poll for new tracer strings.
 function websocketConnect() {
   const nws = new WebSocket(`ws://${restServer}/ws`);
   nws.addEventListener("message", function(event) {
@@ -152,11 +168,13 @@ function websocketConnect() {
   });
 
   nws.addEventListener("close", function() {
-    setTimeout(websocketConnect, 1500); // Attempt to reconnect when the socket closes.
+    // Attempt to reconnect when the socket closes.
+    setTimeout(websocketConnect, 1500);
   });
 }
 
-/* Query the configuration. */
+// configQuery returns the appropriate configuration information
+// that is requested from the content script.
 function configQuery(message, sender, sendResponse) {
   if (message && message.config) {
     switch (message.config) {
@@ -173,7 +191,7 @@ function configQuery(message, sender, sendResponse) {
   }
 }
 
-/* Add a job to the job queue. */
+// Add a job to the job queue.
 function addJobToQueue(message, sender, sendResponse) {
   // If it is the first job added, set a timer to process the jobs.
   if (jobs.length === 0) {
@@ -182,44 +200,27 @@ function addJobToQueue(message, sender, sendResponse) {
   jobs.push(message);
 }
 
-/* Global list of DOM writes. Periodically, this will be sent to the background thread and cleared. */
+// Global list of DOM writes. When a job is written to this array
+// the background page will wait a few seconds collecting more jobs
+// and then send them all off to the API.
 let jobs = [];
 
-/* Process all the jobs in the current queue. */
+// Process all the jobs in the current queue.
 function processDomEvents() {
-  /* If there are no new jobs, continue. */
-  if (enabled) {
-    const p = JSON.parse(JSON.stringify(jobs));
-    /* Clear out the jobs. */
-    jobs = [];
-    /* Send any jobs off to the API server. */
-    requestHandler(p);
-  }
+  const p = JSON.parse(JSON.stringify(jobs));
+  // Clear out the jobs.
+  jobs = [];
+  // Send any jobs off to the API server.
+  requestHandler(p);
 }
 
-/* Any time the page sends a message to the extension, the above handler should 
- * take care of it. */
+// Any time the page sends a message to the extension, the above handler should
+// take care of it.
 chrome.runtime.onMessage.addListener(messageRouter);
-/* Any time the UI tries to check if the extension is called, it will make an 
- * external message. */
+// Any time the UI tries to check if the extension is called, it will make an
+// external message.
 chrome.runtime.onMessageExternal.addListener((r, s, _) => {
   return true;
-});
-chrome.browserAction.onClicked.addListener(function(tab) {
-  enabled = !enabled;
-  if (!enabled) {
-    chrome.browserAction.setIcon({
-      path: {
-        16: "images/tracy_16x16_x.png"
-      }
-    });
-  } else {
-    chrome.browserAction.setIcon({
-      path: {
-        16: "images/tracy_16x16.png"
-      }
-    });
-  }
 });
 
 // Configuration defaults
@@ -227,5 +228,5 @@ let restServer = "127.0.0.1:443";
 let tracerStringTypes = ["Can't connect to API. Is Tracy running?"];
 let defaultTracer = "";
 let tracerPayloads = [];
-let enabled = true;
+
 refreshConfig(true);
--- plugin/scripts/options.js
+++ plugin/scripts/options.js
@@ -1,15 +1,19 @@
 // Saves options to chrome.storage
 function saveOptions() {
-  let restHost = document.getElementById("rest-host").value;
-  let restPort = document.getElementById("rest-port").value;
+  const restHost = document.getElementById("rest-host").value;
+  const restPort = document.getElementById("rest-port").value;
+  const autoFill = document.getElementById("auto-fill").checked;
+  const autoFillPayload = document.getElementById("auto-fill-dropdown").value;
   chrome.storage.local.set(
     {
       restHost: restHost,
-      restPort: restPort
+      restPort: restPort,
+      autoFill: autoFill,
+      autoFillPayload: autoFillPayload
     },
-    function() {
+    () => {
       // Update status to let user know options were saved.
-      let status = document.getElementById("status");
+      const status = document.getElementById("status");
       status.textContent = "Options saved.";
       setTimeout(function() {
         status.textContent = "";
@@ -24,13 +28,41 @@ function restoreOptions() {
   chrome.storage.local.get(
     {
       restHost: "localhost",
-      restPort: 8081
+      restPort: 8081,
+      autoFill: false,
+      autoFillPayload: "GEN-XSS"
     },
-    function(res) {
-      document.getElementById("rest-host").value = res.restHost;
-      document.getElementById("rest-port").value = res.restPort;
+    settings => {
+      const s = document.getElementById("auto-fill-dropdown");
+      fetch(`http://${settings.restHost}:${settings.restPort}/config`, {
+        headers: { Hoot: "!" }
+      })
+        .then(res => res.json())
+        .catch(err => console.error(err))
+        .then(res => {
+          Object.keys(res["tracers"]).forEach(i => {
+            const o = document.createElement("option");
+            o.text = i;
+            s.add(o);
+          });
+
+          console.log("[PAYLOAD]", settings);
+          document.getElementById("auto-fill-dropdown").value =
+            settings.autoFillPayload;
+        });
+
+      document.getElementById("rest-host").value = settings.restHost;
+      document.getElementById("rest-port").value = settings.restPort;
+      document.getElementById("auto-fill").checked = settings.autoFill;
+      s.disabled = !settings.autoFill;
     }
   );
 }
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.getElementById("save").addEventListener("click", saveOptions);
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("save").addEventListener("click", saveOptions);
+  document.getElementById("auto-fill").addEventListener("click", e => {
+    const t = document.getElementById("auto-fill-dropdown");
+    t.disabled = !t.disabled;
+  });
+  restoreOptions();
+});
